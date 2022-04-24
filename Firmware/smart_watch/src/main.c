193729954482a8a8f68dcd3d36386d5723dd08e1
jasonskells <jason.skells@uwaterloo.ca>
2022-04-24 03:15:34 -0400

    bunch of changes to the schematic and its almost finished - tried to program the esp32 in vscode but couldn't get that working yet

--- Firmware/smart_watch/src/main.c
+++ Firmware/smart_watch/src/main.c
@@ -1,1 +1,18 @@
-void app_main() {}
+#include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "driver/gpio.h"
+#include "sdkconfig.h"
+
+void app_main()
+{
+
+    gpio_pad_select_gpio();
+    gpio_set_direction(, GPIO_MODE_OUTPUT);
+    while(1) {
+        gpio_set_level(, 0);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        gpio_set_level(, 1);
+        vTaskDelay(1000 / portTICK_PERIOD_MS);
+    }
+}
