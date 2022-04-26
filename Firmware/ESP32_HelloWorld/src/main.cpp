1255263a60a2c576a1afb9e9e313235260dfab8e
Your Name <Joeyjchen@hotmail.com>
2022-04-26 01:11:06 -0400

    Add GPIO

--- Firmware/ESP32_HelloWorld/src/main.cpp
+++ Firmware/ESP32_HelloWorld/src/main.cpp
@@ -7,5 +7,9 @@ extern "C"
     {
         static const char *TAG = "MAIN";
         ESP_LOGD(TAG, "Hello World");
+
+        gpio_pad_select_gpio(GPIO_NUM_0);
+        ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_0, GPIO_MODE_OUTPUT));
+        ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_0, 1));
     }
 }
