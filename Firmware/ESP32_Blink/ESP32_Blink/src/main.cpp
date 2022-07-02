9de9f16d816c800d0630ad4d043c655e81fb52f8
Your Name <Joeyjchen@hotmail.com>
2022-07-02 19:59:55 -0400

    Change Hello World to WiFi connection:

--- Firmware/ESP32_Blink/ESP32_Blink/src/main.cpp
+++ Firmware/ESP32_Blink/ESP32_Blink/src/main.cpp
@@ -13,7 +13,15 @@ extern "C"
         {
             vTaskDelay(500 / portTICK_RATE_MS);
             ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_0, 0));
-            ESP_LOGD(TAG, "Switching the LED");
+            ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_1, 0));
+            ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_2, 0));
+            ESP_LOGD(TAG, "Switching OFF the LED");
+            vTaskDelay(500 / portTICK_RATE_MS);
+            ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_0, 1));
+            ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_1, 1));
+            ESP_ERROR_CHECK(gpio_set_level(GPIO_NUM_2, 1));
+
+            ESP_LOGD(TAG, "Switching ON the LED");
         }
     }
 
@@ -23,6 +31,12 @@ extern "C"
 
         gpio_pad_select_gpio(GPIO_NUM_0);
         ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_0, GPIO_MODE_OUTPUT));
+
+        gpio_pad_select_gpio(GPIO_NUM_1);
+        ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_1, GPIO_MODE_OUTPUT));
+
+        gpio_pad_select_gpio(GPIO_NUM_2);
+        ESP_ERROR_CHECK(gpio_set_direction(GPIO_NUM_2, GPIO_MODE_OUTPUT));
         blink();
     }
 }
